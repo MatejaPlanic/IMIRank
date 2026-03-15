a6c294a552e7c600595539bc9d4dc787988056a1
Mateja Planic <matejaplanic123@gmail.com>
2026-03-15 21:56:13 +0100

    Napravljen front deo za home page

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Register } from './register/register';
 import { Login } from './login/login';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path : 'login',
         component: Login
+    },
+    {
+        path:'home',
+        component: Home
     }
 ];
